21ed32b4c76cc4fd395b26a4088e5c5c44a2709d
safa alipoor <safaalipoor1431@gmail.com>
2021-12-30 22:21:27 +0330

    updated 10:21 pm

--- src/controller/CreateChannelController.java
+++ src/controller/CreateChannelController.java
@@ -1,11 +1,28 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import model.User;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CreateChannelController implements Initializable {
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private TableView<User> userList;
+
+    @FXML
+    private TextField channelName;
+
+    @FXML
+    private Button createButton;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
