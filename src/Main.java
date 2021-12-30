0a14b4c1ae5578666e286d1ffbee5a9be3c4cb8c
safa alipoor <safaalipoor1431@gmail.com>
2021-12-30 22:09:15 +0330

    new project

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("view/loginpage.fxml"));
+        loader.load();
+        primaryStage.setTitle("LoginPage");
+        primaryStage.setScene(new Scene(loader.getRoot()));
+        primaryStage.show();
+
+    }
+}
---
+++ src/model/User.java
@@ -0,0 +1,73 @@
+package model;
+
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String phoneNumber;
+    static ArrayList<User> userArrayList = new ArrayList<>();
+
+
+
+    public User(String name, String lastName, String userName, String password, String phoneNumber) {
+        this.name = name;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public User() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public ArrayList<User> getUserArrayList() {
+        return userArrayList;
+    }
+
+    public void setUserArrayList(ArrayList<User> userArrayList) {
+        this.userArrayList = userArrayList;
+    }
+}
